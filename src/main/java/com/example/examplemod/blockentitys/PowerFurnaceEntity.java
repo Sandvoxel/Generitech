9c21bd73896b1ca1dc5e97f7599e712b343665d5
Kyle Kovalchick <Kovalchickzz@gmail.com>
2021-12-03 14:57:31 -0600

    Simple changes

--- src/main/java/com/example/examplemod/blockentitys/PowerFurnaceEntity.java
+++ src/main/java/com/example/examplemod/blockentitys/PowerFurnaceEntity.java
@@ -1,8 +1,10 @@
 package com.example.examplemod.blockentitys;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.energy.IEnergyStorage;
 
 public class PowerFurnaceEntity extends BlockEntity{
 
@@ -10,4 +12,7 @@ public class PowerFurnaceEntity extends BlockEntity{
         super(BlockEntities.POWER_FURNACE.getEntityType(), blockPos, blockState);
     }
 
+
+    public static <T extends BlockEntity> void tick(Level level, BlockPos blockPos, BlockState blockState, T t) {
+    }
 }
--- src/main/java/com/example/examplemod/blocks/PowerFurnaceBlock.java
+++ src/main/java/com/example/examplemod/blocks/PowerFurnaceBlock.java
@@ -1,10 +1,21 @@
 package com.example.examplemod.blocks;
 
+import com.example.examplemod.Generitech;
+import com.example.examplemod.blockentitys.BlockEntities;
 import com.example.examplemod.blockentitys.PowerFurnaceEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Wearable;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
 
 public class PowerFurnaceBlock extends BlockEntityBase implements Wearable {
 
@@ -12,6 +23,18 @@ public class PowerFurnaceBlock extends BlockEntityBase implements Wearable {
 
     public PowerFurnaceBlock() {
         super(Properties.of(Material.METAL), "power_furnace", PowerFurnaceEntity::new);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> blockEntityType) {
+        Generitech.LOGGER.info("How offen is this called?");
+        return BlockEntities.POWER_FURNACE.getEntityType() == blockEntityType ? PowerFurnaceEntity::tick : null;
+    }
+
+    @Override
+    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
+        return super.use(blockState, level, blockPos, player, interactionHand, blockHitResult);
     }
 
 }
